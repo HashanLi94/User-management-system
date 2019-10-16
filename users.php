b3ea84ea7bea32e805ff8d801a66020e077f4a69
HashanLi94 <hashanduplessis@gmail.com>
2019-10-17 02:35:37 +0530

    users page

---
+++ users.php
@@ -0,0 +1,22 @@
+<?php include_once('includes/connection.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="stylesheet" href="css\main.css"> -->
+    <title>UMS-Users_page</title>
+</head>
+<body>
+   USERS
+
+    
+
+
+ 
+    
+</body>
+</html>
+<?php mysqli_close($connection); ?>
