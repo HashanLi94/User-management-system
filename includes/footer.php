139e5276d30864df51356bc86f681ded11aaa07d
HashanLi94 <hashanduplessis@gmail.com>
2019-10-18 02:50:33 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>footer.php</title>
+</head>
+<body>
+    <center>HashDev1.0 | 2019/2020 | All rights received | UMS </center>
+    
+</body>
+</html>
