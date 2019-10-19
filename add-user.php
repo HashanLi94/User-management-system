bf5cf92369bc94349b47f958510df5d8d976f5a3
HashanLi94 <hashanduplessis@gmail.com>
2019-10-19 22:48:37 +0530

    Update add-user.php

--- add-user.php
+++ add-user.php
@@ -9,6 +9,41 @@
 
 	
  ?>
+
+ <?php
+    //checking the fields are okay
+    $errors = array();
+    if(isset($_POST['submit'])){
+       
+
+        if(trim(empty($_POST['first_name']))){
+              $errors[] = 'First name is required..!!!';
+        }
+
+        if(trim(empty($_POST['last_name']))){
+            $errors[] = 'Last_name required..!!!';
+        }
+
+        if(trim(empty($_POST['email']))){
+            $errors[] = 'email is required..!!!';
+        }
+
+        if(trim(empty($_POST['password']))){
+            $errors[] = 'Password is required..!!!';
+         }
+
+         //using foreach loop
+         /*$req_fields = array('first_name', 'last_name', 'email', 'password');
+
+         foreach($req_fields as $filed){
+            if(trim(empty($_POST['$filed']))){
+                $errors[] = $filed . " is required";
+            }
+         }*/
+
+    }
+ 
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,27 +59,37 @@
 
 	<main>
         <h1>Add New User<span><a href="users.php">< back to Users</a></span></h1>
+
+        <?php
+            if(!empty($errors)){
+                echo '<div class = "errmsgs">';
+                echo "<b>There was an error in the filling.</b> <br>";
+                foreach($errors as $error){
+                    echo "-". $error ."<br>";
+                }
+                echo '</div>';
+            }
+        ?>
         
         <form action="add-user.php" class="userform" method="post">
             <p>
                 <label for="">First_name</label>
-                <input type="text" name=first_name required>
+                <input type="text" name=first_name >
             </p>
 
             <p>
                 <label for="">Last_name</label>
-                <input type="text" name="last_name" required>
+                <input type="text" name="last_name" >
             </p>
 
             <p>
                 <label for="">E-mail address</label>
-                <input type="email" name="email" required>
+                <input type="email" name="email" >
             </p>
 
             <p>
                 <label for="">Passowrd</label>
-                <input type="password" name="password" required>
-            </p>
+                <input type="password" name="password">
 
             <p>
                 <label for="">&nbsp;</label>
