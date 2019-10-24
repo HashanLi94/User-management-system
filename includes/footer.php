8242590f0c6f13d079eeae395ae7f288353847dd
HashanLi94 <hashanduplessis@gmail.com>
2019-10-24 12:31:41 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
     <title>footer.php</title>
 </head>
 <body>
-    <hr>
-    <center>HashDev1.0 | 2019/2020 | All rights received | UMS </center>
-    <hr>
+   
+    <center><img src="img\redis-logo.png" alt="" width="400px" height="150px" ></center>
+    <center>redis 1.0 | 2019/2020 | All rights received | UMS </center>
+    <!-- style="opacity:0.4;filter:alpha(opacity=40);" -->
+
+    
+    
 </body>
 </html>
